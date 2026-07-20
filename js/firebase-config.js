33fc495aee2601fa9650aad99c8869902f623d27
Subham.XD <subham.panda.dev@gmail.com>
2026-07-20 19:01:43 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,13 @@
 // Reuse the EXACT SAME config object you already use on your login/signup page.
 // (Same Firebase project — so favourites saved here show up everywhere in CorDex.)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
+  apiKey: "AIzaSyDeTSmE9dVlmyU1vgFjw6fDF4bvk6ssUUY",
   authDomain: "devpandaxd-default-rtdb.firebaseapp.com",
   databaseURL: "https://devpandaxd-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "devpandaxd",
   storageBucket: "devpandaxd.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  messagingSenderId: "947689099132",
+  appId: "1:947689099132:web:495aaf1c69f75a11f2def9"
 };
 
 firebase.initializeApp(firebaseConfig);
